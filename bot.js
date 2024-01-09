78f21604cdab8edf57768ee07b33642965179215
SaahilSabu <saahil.sabu@gmail.com>
2024-01-09 20:01:05 +0530

    Fetch user list on leave from bamboohr

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const { App } = require('@slack/bolt');
 const { CronJob } = require('cron');
+const axios = require('axios');
 require('dotenv').config();
-
 
 const app = new App({
   token: process.env.BOT_TOKEN,
@@ -10,23 +10,62 @@ const app = new App({
 
 async function sendMessage(channelId, message) {
   try {
-    await app.client.chat.postMessage({
+    const result = await app.client.chat.postMessage({
       token: process.env.BOT_TOKEN,
       channel: channelId,
       text: message,
     });
+
+    if (!result.ok) {
+      console.error(`Failed to send message: ${result.error}`);
+    }
   } catch (error) {
-    console.error(error);
+    console.error('Error sending message:', error.message);
   }
 }
 
-const job = new CronJob('00 51 12 * * *', () => {
-    channelId = process.env.CHANNEL_ID
-    const message = 'Checking to see if I can run every day at 12:51 PM IST';
-    sendMessage(channelId, message);
-}, null, true, 'Asia/Kolkata');
 
-job.start();
+async function getLeaveInfo() {
+  try {
+    const response = await axios.get(
+      'https://api.bamboohr.com/api/gateway.php/branch/v1/time_off/whos_out',
+      {
+        headers: {
+          Authorization: `Basic ${Buffer.from(
+            `${process.env.BAMBOOHR_API_KEY}:x`
+          ).toString('base64')}`,
+          Accept: 'application/json',
+        },
+      }
+    );
+
+    const leaveData = response.data;
+
+    const usersOnLeave = leaveData
+      .map((entry) => entry.name);
+
+    const message =
+      usersOnLeave.length > 0
+        ? `Users on leave:\n${usersOnLeave.join('\n')}`
+        : 'No users are currently on leave.';
+
+        console.log(message)
+
+    const channelId = process.env.CHANNEL_ID;
+    sendMessage(channelId, message);
+  } catch (error) {
+    console.error('Error fetching BambooHR data:', error.message);
+  }
+}
+
+// const job = new CronJob('* 00 10 * * *', () => {
+//   getLeaveInfo();
+// }, null, true, 'Asia/Kolkata');
+
+// job.start();
+
+
+getLeaveInfo();
 
 (async () => {
   await app.start(process.env.PORT || 3000);
